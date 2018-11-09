32d6448d85ece34e384b1d8f35b5c052e9d07cd9
Achilles Moraites <achimoraites@yahoo.gr>
2018-11-10 00:13:01 +0200

    added reducers

---
+++ client/src/components/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+
+    render() {
+        return (
+            <div>
+                App component
+        </div>
+        );
+
+    }
+}
+
+export default App;
