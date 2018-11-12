b5e17117efa42b70af1a281392dba79019a77ae9
Achilles Moraites <achimoraites@yahoo.gr>
2018-11-12 14:35:47 +0200

    landing component added

---
+++ client/src/components/Landing.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Landing{
+
+    render(){
+        return (
+            <div>
+                <h2>Landing</h2>
+            </div>
+        )
+    }
+}
+
+export default Landing;
