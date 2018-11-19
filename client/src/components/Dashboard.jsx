b939a194b050151f11d87dc6408eb39a5eb91610
Achilles Moraites <achimoraites@yahoo.gr>
2018-11-19 21:13:07 +0200

    tested mailer: OK

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -22,17 +22,17 @@ class Dashboard extends Component {
                         </div>
                     </div>
 
-                    <div class="col s12 m7">
-                        <h2 class="header">Horizontal Card</h2>
-                        <div class="card horizontal">
-                            <div class="card-image">
-                                <img src="https://lorempixel.com/100/190/nature/6" />
+                    <div className="col s12 m7">
+                        <h2 className="header">Horizontal Card</h2>
+                        <div className="card horizontal">
+                            <div className="card-image">
+                                <img alt="" src="https://lorempixel.com/100/190/nature/6" />
                             </div>
-                            <div class="card-stacked">
-                                <div class="card-content">
+                            <div className="card-stacked">
+                                <div className="card-content">
                                     <p>I am a very simple card. I am good at containing small bits of information.</p>
                                 </div>
-                                <div class="card-action">
+                                <div className="card-action">
                                     <a href="#">This is a link</a>
                                 </div>
                             </div>
--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
-requires('./routes/surveyRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
   // serve up production assets
