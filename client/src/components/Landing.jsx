8a823dd9f5c33a6c04d436b1c6ad836b5a921280
Achilles Moraites <achimoraites@yahoo.gr>
2018-11-20 14:52:36 +0200

    landing is ready

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -22,15 +22,16 @@ class Landing extends Component {
             <div className="section">
               <div className="row">
                 <div className="col s12 m4">
-                  <div className="icon-block">
+                  <div className="icon-block center">
+                  <i className="large material-icons">blur_on</i>
                     <h5 className="center">Easy Data Collection</h5>
-        
                     <p className="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
                   </div>
                 </div>
         
                 <div className="col s12 m4">
-                  <div className="icon-block">
+                  <div className="icon-block center">
+                  <i className="large material-icons">assessment</i>
                     <h5 className="center">User Experience Focused</h5>
         
                     <p className="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
@@ -38,7 +39,8 @@ class Landing extends Component {
                 </div>
         
                 <div className="col s12 m4">
-                  <div className="icon-block">
+                  <div className="icon-block center">
+                  <i className="large material-icons">mood</i>
                     <h5 className="center">Easy to work with</h5>
         
                     <p className="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
