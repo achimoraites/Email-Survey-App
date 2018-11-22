72e49c3555ee16751eea6b43e178d40c14e5d765
Achilles Moraites <achimoraites@yahoo.gr>
2018-11-22 16:23:45 +0200

    styled the add survey btn

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -8,7 +8,7 @@ class Dashboard extends Component {
             <div style={{ marginTop: "5rem" }} className="container">
              
 
-                    <div className="fixed-action-btn">
+                    <div style={{right:"25%",bottom: "25%"}} className="fixed-action-btn">
                         <Link to={'/surveys/new'} className="btn-floating btn-large red">
                             <i className="large material-icons">add</i>
                         </Link>
