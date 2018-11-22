107bf96f84f1957eb86b5781d30d264a84eb8cf5
Achilles Moraites <achimoraites@yahoo.gr>
2018-11-22 19:24:27 +0200

    survey form can validate emails

--- client/src/components/surveys/SurveyForm.jsx
+++ client/src/components/surveys/SurveyForm.jsx
@@ -4,6 +4,7 @@ import { reduxForm, Field } from 'redux-form';
 import _ from 'lodash';
 import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
+import validateEmails from  '../../utils/validateEmails';
 
 const FIELDS = [
     {label: 'Survey Title', name : 'title'},
@@ -41,6 +42,8 @@ class SurveyForm extends Component {
 function validate(values) {
     const errors = {};
 
+    errors.emails = validateEmails(values.emails);
+    
     _.each(FIELDS, ({ name }) =>{
         if (!values[name]) {
             errors[name] = `${name} cannot be empty !`;
--- client/src/utils/validateEmails.js
+++ client/src/utils/validateEmails.js
@@ -1,5 +1,7 @@
+import { S_IFMT } from "constants";
+
 const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-export default (emails) => {
+export default (emails = '') => {
 
     const invalidEmails = emails
     .split(',')
