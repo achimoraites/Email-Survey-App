986d4b81f53739b8b7bf623a60b63e31717e7b10
Achilles Moraites <achimoraites@yahoo.gr>
2018-12-02 22:10:53 +0200

    delete survey implemented on back end

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,6 +1,8 @@
 const _ = require('lodash');
 const Path = require('path-parser');
-const { URL } = require('url');
+const {
+    URL
+} = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
@@ -9,88 +11,118 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
 
-module.exports = app =>{
+module.exports = app => {
 
-   
-
-    app.delete('/api/surveys/:surveyId', requireLogin , async (req, res)=> {
-        const user = req.user;
-        const deleteSurvey = await Survey.findOneAndDelete({
-            _id : surveyId
-        }).exec();
-        res.send(deleteSurvey);
-    });
-
-    app.get('/api/surveys/:surveyId/:choice', (req, res) =>{
+    app.get('/api/surveys/:surveyId/:choice', (req, res) => {
         res.send(`Thank you for voting!!!`);
     });
 
     app.post('/api/surveys/webhooks', (req, res) => {
         const p = new Path('/api/surveys/:surveyId/:choice');
-       _.chain(req.body)
-        .map( (event) => {
-            const match = p.test(new URL(event.url).pathname);
-            if (match) {
-                const { email } = event;
-                const { surveyId, choice } = match;
-                return { email, surveyId, choice };
-            }
-        })
-        .compact()
-        .uniqWith(_.isEqual)
-        .each((event) => {
-            console.log('choice', event.choice);
-          
-            Survey.updateOne(
-                {
-                  _id: event.surveyId,
-                  recipients: {
-                    $elemMatch: { email: event.email, clicked: false }
-                  }
-                },
-                {
-                  $inc: { [event.choice]: 1 },
-                  $set: { 'recipients.$.clicked': true },
-                  lastResponded: new Date()
+        _.chain(req.body)
+            .map((event) => {
+                const match = p.test(new URL(event.url).pathname);
+                if (match) {
+                    const {
+                        email
+                    } = event;
+                    const {
+                        surveyId,
+                        choice
+                    } = match;
+                    return {
+                        email,
+                        surveyId,
+                        choice
+                    };
                 }
-      ).exec();         
-        })
-        .value();
+            })
+            .compact()
+            .uniqWith(_.isEqual)
+            .each((event) => {
+                console.log('choice', event.choice);
+
+                Survey.updateOne({
+                    _id: event.surveyId,
+                    recipients: {
+                        $elemMatch: {
+                            email: event.email,
+                            clicked: false
+                        }
+                    }
+                }, {
+                    $inc: {
+                        [event.choice]: 1
+                    },
+                    $set: {
+                        'recipients.$.clicked': true
+                    },
+                    lastResponded: new Date()
+                }).exec();
+            })
+            .value();
         res.send({});
     });
 
+   
+
     app.route('/api/surveys')
-    .get(requireLogin, async (req, res)=> {
-        const user = req.user;
-        const surveys = await Survey.find({
-            _user: user
+        .get(requireLogin, async (req, res) => {
+            const user = req.user;
+            const surveys = await Survey.find({
+                    _user: user
+                })
+                .select({
+                    recipients: false
+                });
+            res.send(surveys);
         })
-        .select({recipients: false});
-        res.send(surveys);
-    })
-    .post(requireLogin, requireCredits, async (req, res) => {
-        const { title, subject, body, recipients } = req.body;
-      
-        const survey = new Survey({
-            title, 
-            subject, 
-            body, 
-            recipients : recipients.split(',').map(email => ({email: email.trim()})),
-            _user : req.user.id,
-            dateSent: Date.now()
+        .post(requireLogin, requireCredits, async (req, res) => {
+            const {
+                title,
+                subject,
+                body,
+                recipients
+            } = req.body;
+
+            const survey = new Survey({
+                title,
+                subject,
+                body,
+                recipients: recipients.split(',').map(email => ({
+                    email: email.trim()
+                })),
+                _user: req.user.id,
+                dateSent: Date.now()
+            });
+
+            // send the emails
+            const mailer = new Mailer(survey, surveyTemplate(survey));
+            try {
+                await mailer.send();
+                await survey.save();
+                req.user.credits -= 1;
+                const user = await req.user.save();
+                console.log(user);
+                res.send(user);
+            } catch (err) {
+                res.status(422).send(err);
+            }
         });
 
-        // send the emails
-        const mailer = new Mailer(survey, surveyTemplate(survey));
+        
+    app.delete('/api/surveys/:surveyId', requireLogin, async (req, res) => {
+        console.log(req.params.surveyId);
+        const surveyId = req.params.surveyId;
         try {
-            await mailer.send();
-            await survey.save();
-            req.user.credits -= 1;
-            const user = await req.user.save();
-            console.log(user);
-            res.send(user);
+            const deleteSurvey = await Survey.findOneAndDelete({
+                _id: surveyId
+            }).exec();
+            res.send(deleteSurvey);
         } catch (err) {
-            res.status(422).send(err);
+            res.status(400).send(err);
         }
+       
     });
+
 }
